223e1407d1a659107e2b1e6d86d6a98ee5b30cd0
krishnaop8 <krishnaop8@gmail.com>
2017-02-07 20:44:46 +0530

    Added the navigation titles

--- app/src/main/java/com/example/omprakash/solus/MainActivity.java
+++ app/src/main/java/com/example/omprakash/solus/MainActivity.java
@@ -85,17 +85,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_event) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_workshop) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_pro_night) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_blog) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_about) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_contact) {
 
         }
 
