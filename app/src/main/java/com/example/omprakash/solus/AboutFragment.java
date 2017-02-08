3e28b67367bd289083412906bee523c1b0555639
krishnaop8 <krishnaop8@gmail.com>
2017-02-08 20:12:52 +0530

    Completed Events,About,Login Fragment

--- app/src/main/java/com/example/omprakash/solus/AboutFragment.java
+++ app/src/main/java/com/example/omprakash/solus/AboutFragment.java
@@ -1,12 +1,19 @@
 package com.example.omprakash.solus;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.tomer.fadingtextview.FadingTextView;
+
+import jp.wasabeef.blurry.Blurry;
+
 public class AboutFragment extends Fragment {
 
 
@@ -18,7 +25,7 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_about, container, false);
-
+        ImageView techofesLogo= (ImageView)view.findViewById(R.id.techofesLogo);
         return view;
     }
 
--- app/src/main/java/com/example/omprakash/solus/MainActivity.java
+++ app/src/main/java/com/example/omprakash/solus/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.omprakash.solus;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -95,14 +96,27 @@ public class MainActivity extends AppCompatActivity
 
         return super.onOptionsItemSelected(item);
     }
+    public void login()
+    {
+        LoginFragment loginFragment = new LoginFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.content_main,loginFragment,loginFragment.getTag()).commit();
+    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        if(id == R.id.userImage)
+        {
+            login();
+        }
+        if(id == R.id.nav_home){
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+            startActivity(intent);
 
-
+        }
         if (id == R.id.nav_event) {
                 EventFragment eventFragment = new EventFragment();
             FragmentManager manager = getSupportFragmentManager();
